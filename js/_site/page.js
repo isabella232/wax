22de7649fdba840321cc6671cf11a0875b8985c9
Tom MacWright <tom@macwright.org>
2014-03-10 10:11:05 -0400

    RIP

--- js/_site/page.js
+++
@@ -1,7 +0,0 @@
-// Application bootstrap.
-$.domReady(function() {
-    $('<div class="demo"></div>')
-        .insertAfter('div.live')
-        .html($('div.live').text())
-        .map(function(elem) { console.log(elem); eval($('script', elem[0]).text()); });
-});
