3d78868b3b0bf1bbcb9725fe48d8baffc4baa56c
Tom MacWright <tom@macwright.org>
2014-03-10 10:08:34 -0400

    Push to mapbox.js

--- README.md
+++
@@ -1,4 +0,0 @@
-# Wax
-
-This is the gh-pages branch of Wax: you won't find source code here. But do
-check it out if you want to edit the Wax website.
--- js/page.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    var pres = document.getElementsByTagName('pre');
-    for (var i = 0; i < pres.length; i++) {
-      if (pres[i].className.match(/live/g)) {
-        eval(pres[i].innerHTML);
-      }
-    }
-    prettyPrint();
-};
